bcc39cd660f9fae740dc8502651453e86713469e
John Hane <Johnny@Johnnys-MBP-121.home>
2019-08-07 16:12:15 -0400

    trying out git

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,11 +1,9 @@
 import React from 'react';
+import { shallow } from 'enzyme';
 import Header from '../../components/Header';
 
 test('should render Header correctly', () => {
   const wrapper = shallow(<Header />);
-  expect(wrapper.find('h1').text()).toBe('Munny');
-  // const renderer = new ReactShallowRenderer();
-  // renderer.render(<Header />);
-  // expect(renderer.getRenderOutput()).toMatchSnapshot();
+  expect(wrapper).toMatchSnapshot();
 })
 
